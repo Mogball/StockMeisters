48f5a174057e6bf750c885daa9dd4840caff8d9c
Jeff Niu <jeffniu22@gmail.com>
2018-01-26 20:48:18 -0500

    Initial commit

---
+++ src/main/java/com/waterloo/oec/Main.java
@@ -0,0 +1,8 @@
+package com.waterloo.oec;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Util.println("Hello");
+    }
+}
---
+++ src/main/java/com/waterloo/oec/Util.java
@@ -0,0 +1,12 @@
+package com.waterloo.oec;
+
+/**
+ * Created by Jeff Niu
+ */
+public class Util {
+
+	public static void println(Object o) {
+		System.out.println(o);
+	}
+
+}
